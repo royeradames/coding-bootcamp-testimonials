11a0fc79b9d9f0307a5965b6fa619263dd73fd00
royeradames <royeraadames@gmail.com>
2021-03-31 21:06:26 -0400

    📦import inter font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <article className="testimonials"> </article>;
+  return (
+    <article className="testimonials">
+      {" "}
+      <header>testing</header>{" "}
+    </article>
+  );
 }
 
 export default App;
