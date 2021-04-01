0eff807bfc2729b56816e8ee8a211254ff86f09e
royeradames <royeraadames@gmail.com>
2021-03-31 22:59:57 -0400

    📦add images to the src file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import "./testimonials-css/mobile.scss";
 import "./testimonials-css/desktop.scss";
 
+// import svgs
+import { ReactComponent as IconNext } from "./images/icon-next.svg";
+import { ReactComponent as InconPrev } from "./images/icon-prev.svg";
+import { ReactComponent as PatternBg } from "./images/pattern-bg.svg";
+import { ReactComponent as PatternCurve } from "./images/pattern-curve.svg";
+import { ReactComponent as PatternQuotes } from "./images/pattern-quotes.svg";
+
 function App() {
   return (
-    <article className="testimonials">
-      {" "}
-      <header>testing</header>{" "}
-    </article>
+    <>
+      {/* add tags inside the main tag */}
+      <PatternBg className="page__pattern-bg" />
+      <PatternQuotes className="page__pattern-quotes" />
+      <PatternCurve className="page__pattern-curve" />
+
+      <article className="testimonials">
+        <header></header>
+      </article>
+    </>
   );
 }
 
