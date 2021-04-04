4d15b5330aabd7e15e7d22b5282dbcf3ac6ca726
royeradames <royeraadames@gmail.com>
2021-04-03 20:13:50 -0400

    👌handle the complex background with bacdkground images

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,13 @@ import React from "react";
 import "./testimonials-css/mobile.scss";
 import "./testimonials-css/desktop.scss";
 
-// import svgs
-import { ReactComponent as IconNext } from "./images/icon-next.svg";
-import { ReactComponent as InconPrev } from "./images/icon-prev.svg";
-import { ReactComponent as PatternBg } from "./images/pattern-bg.svg";
-import { ReactComponent as PatternCurve } from "./images/pattern-curve.svg";
-import { ReactComponent as PatternQuotes } from "./images/pattern-quotes.svg";
-
 function App() {
   return (
     <>
       {/* add tags inside the main tag */}
-      <PatternBg className="page__pattern-bg" />
-      <PatternQuotes className="page__pattern-quotes" />
-      <PatternCurve className="page__pattern-curve" />
 
       <article className="testimonials">
-        <header></header>
+        <header>{/* <h1>Testing</h1> */}</header>
       </article>
     </>
   );
